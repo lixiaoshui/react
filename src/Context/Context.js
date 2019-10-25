95ab1b7b1fa123a8fb51adfb47595612c9ae7a4d
lixiaoshui <2087751927@qq.com>
2019-10-25 21:17:05 +0800

    lijia

--- src/Context/Context.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-// export let {Provider,Consumer} = React.createContent()
-export let con = React.createContext();
-export let con2 = React.createContext();
--- src/NoMatch.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-export default function NoMatch(){
-    return <h1>你的页面飞走了</h1>;
-}
